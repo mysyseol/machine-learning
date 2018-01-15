2135f62376178782af1d97df54683fd1e8ef32b4
mysyseol <35212932+mysyseol@users.noreply.github.com>
2018-01-15 16:15:07 +0900

    Update spiral_numpy.py

--- spiral_numpy.py
+++ spiral_numpy.py
@@ -1,9 +1,10 @@
 # spiral.py
 # numpy version of classifier
-# 
+ 
 import numpy as np
 import matplotlib.pyplot as plt
 
+# data preparation
 def input_data():
     N = 100 # number of points per class
     D = 2 # dimensionality
@@ -22,6 +23,7 @@ X, y = input_data()
 
 #Train a Linear Classifier
 D, h, K = 2, 100, 3
+
 # initialize parameters randomly
 W = 0.01 * np.random.randn(D,h)
 b = np.zeros((1,h))
@@ -39,34 +41,44 @@ for i in range(10000):
     # evaluate class scores, [N x K]
     hidden_layer = np.maximum(0, np.dot(X, W) + b) # note, ReLU activation
     scores = np.dot(hidden_layer, W2) + b2
+
     # compute the class probabilities
     exp_scores = np.exp(scores)
     probs = exp_scores / np.sum(exp_scores, axis=1, keepdims=True) # [N x K]
+    
     # compute the loss: average cross-entropy loss and regularization
     corect_logprobs = -np.log(probs[range(num_examples),y])
     data_loss = np.sum(corect_logprobs)/num_examples
     reg_loss = 0.5*reg*np.sum(W*W) + 0.5*reg*np.sum(W2*W2)
     loss = data_loss + reg_loss
+    
     if i % 1000 == 0:
         print("iteration %d: loss %f" % (i, loss))
+    
     # compute the gradient on scores
     d_scores = probs
     d_scores[range(num_examples),y] -= 1
     d_scores /= num_examples
+    
     # backpropate the gradient to the parameters
     # first backprop into parameters W2 and b2
     d_W2 = np.dot(hidden_layer.T, d_scores)
     d_b2 = np.sum(d_scores, axis=0, keepdims=True)
+    
     # next backprop into hidden layer
     d_hidden = np.dot(d_scores, W2.T)
+    
     # backprop the ReLU non-linearity
     d_hidden[hidden_layer <= 0] = 0
+    
     # finally into W,b
     d_W = np.dot(X.T, d_hidden)
     d_b = np.sum(d_hidden, axis=0, keepdims=True)
+    
     # add regularization gradient contribution
     d_W2 += reg * W2
     d_W  += reg * W
+    
     # perform a parameter update
     W  += -step_size * d_W
     b  += -step_size * d_b
